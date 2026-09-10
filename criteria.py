5b0e71a62c3a206aec3bfb04d5db277375be9a4f
Pratik Maske <pratik45maske@gmail.com>
2026-09-10 18:55:30 +0530

    eligible or not for discount and must be student

--- criteria.py
+++ criteria.py
@@ -1,3 +1,3 @@
 age = int(input("Enter your age: "))
-student = str(input("Are you a student?, please enter yes or no: "))
+student = (input("Are you a student?, please enter yes or no: "))
 print("you are eligible for discount:", age <= 21 and student == "yes")

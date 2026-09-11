206c1f135c679e09b54f52ffa5ac21de40727934
Pratik Maske <pratik45maske@gmail.com>
2026-09-11 22:15:35 +0530

    python day 2

---
+++ drive.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if( age >=  18):
+    print("You are eligible to drive.")
+    print("thank you")
+else:
+    print("you are not eligible to drive.")
+          
